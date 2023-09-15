32a286f1c6438fed8d516de3e1022cd71a7fff72
Piotr Stefański <stefanskipiotr15@gmail.com>
2023-09-15 14:37:25 +0200

    Parse access flags

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,17 @@
 module Main where
 
-import qualified Data.ByteString.Lazy as B
+import Control.Monad
 import Data.Binary.Get
+import Data.Bits
+import qualified Data.ByteString.Lazy as B
 import Data.Word
+
+validateMagic :: Get Word32
+validateMagic = do
+  magic <- getWord32be
+  if magic /= 0xcafebabe
+    then error "Invalid file format"
+    else return magic
 
 data CPInfo
   = CPClass
@@ -46,7 +55,7 @@ getCPNameAndType = CPNameAndType <$> getWord16be <*> getWord16be
 getCPUtf8 :: Get CPInfo
 getCPUtf8 = do
   count <- fromIntegral <$> getWord16be
-  CPUtf8 <$> (sequence $ replicate count getWord8)
+  CPUtf8 <$> replicateM count getWord8
 
 getCPInfoByTag :: Word8 -> Get CPInfo
 getCPInfoByTag 7 = getCPClass
@@ -66,19 +75,51 @@ type ConstantPool = [CPInfo]
 
 getConstantPool :: Get ConstantPool
 getConstantPool = do
-  count <- ((+) (-1 :: Int)) <$> fromIntegral <$> getWord16be
-  sequence $ replicate count getCPInfo
+  count <- (+) (-1 :: Int) . fromIntegral <$> getWord16be
+  replicateM count getCPInfo
+
+data AccessFlag
+  = AccPublic
+  | AccFinal
+  | AccSuper
+  | AccInterface
+  | AccAbstract
+  | AccSynthetic
+  | AccAnnotation
+  | AccEnum
+  deriving (Show)
+
+accessFlagMap :: [(Word16, AccessFlag)]
+accessFlagMap =
+  [ (0x0001, AccPublic)
+  , (0x0010, AccFinal)
+  , (0x0020, AccSuper)
+  , (0x0200, AccInterface)
+  , (0x0400, AccAbstract)
+  , (0x1000, AccSynthetic)
+  , (0x2000, AccAnnotation)
+  , (0x4000, AccEnum)
+  ]
+
+getAccessFlags :: Get [AccessFlag]
+getAccessFlags = do
+  value <- getWord16be
+  return $ map snd $ filter ((/= 0) . (.&. value) . fst) accessFlagMap
 
 data ClassFile = ClassFile
-  { magic :: Word32
-  , minor :: Word16
+  { minor :: Word16
   , major :: Word16
   , constantPool :: ConstantPool
+  , accessFlags :: [AccessFlag]
   } deriving (Show)
 
 getClassFile :: Get ClassFile
 getClassFile =
-  ClassFile <$> getWord32be <*> getWord16be <*> getWord16be <*> getConstantPool
+  ClassFile
+    <$> (validateMagic *> getWord16be)
+    <*> getWord16be
+    <*> getConstantPool
+    <*> getAccessFlags
 
 parseClassFile :: IO ClassFile
 parseClassFile = do
