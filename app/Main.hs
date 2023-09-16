65bc979eb528d286ee44f885de3b023001443359
Piotr Stefański <stefanskipiotr15@gmail.com>
2023-09-16 16:06:45 +0200

    Run a simple hello world application

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,21 @@
 module Main where
 
+import Codec.Binary.UTF8.String (decode)
+import Control.Applicative
 import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.State.Lazy
 import Data.Binary.Get
 import Data.Bits
 import qualified Data.ByteString.Lazy as B
 import Data.Word
+import Numeric (showHex)
+
+assertWith :: String -> (a -> Bool) -> a -> a
+assertWith msg f a =
+  if f a
+    then a
+    else error msg
 
 validateMagic :: Get Word32
 validateMagic = do
@@ -13,49 +24,77 @@ validateMagic = do
     then error "Invalid file format"
     else return magic
 
+newtype CPRef a = CPRef
+  { getCPRef :: ConstantPool -> (Word16, a)
+  }
+
+instance Functor CPRef where
+  fmap f (CPRef a) =
+    CPRef $ \input -> do
+      let (idx, a') = a input
+      (idx, f a')
+
+instance Show (CPRef a) where
+  show _ = "ConstantPool -> (Word16, a)"
+
+mkCPRefUtf8 :: Word16 -> CPRef String
+mkCPRefUtf8 idx = CPRef $ \cp -> (idx, utfString $ cp !! fromIntegral idx)
+
+mkCPRefCPInfo :: Word16 -> CPRef CPInfo
+mkCPRefCPInfo idx = CPRef $ \cp -> (idx, cp !! fromIntegral idx)
+
 data CPInfo
   = CPClass
-      { clsNameIndex :: Word16
+      { clsName :: CPRef CPInfo
       }
   | CPFieldref
-      { fldrClassIndex :: Word16
-      , fldrNameAndTypeIndex :: Word16
+      { fldrClass :: CPRef CPInfo
+      , fldrNameAndType :: CPRef CPInfo
       }
   | CPMethodref
-      { mthdrClassIndex :: Word16
-      , mthdrNameAndTypeIndex :: Word16
+      { mthdrClass :: CPRef CPInfo
+      , mthdrNameAndType :: CPRef CPInfo
       }
   | CPString
-      { strStringIndex :: Word16
+      { strString :: CPRef CPInfo
       }
   | CPNameAndType
-      { natNameIndex :: Word16
-      , natDescIndex :: Word16
+      { natName :: CPRef CPInfo
+      , natDesc :: CPRef CPInfo
       }
   | CPUtf8
-      { utfBytes :: [Word8]
+      { utfString :: String
       }
   deriving (Show)
 
 getCPClass :: Get CPInfo
-getCPClass = CPClass <$> getWord16be
+getCPClass = CPClass <$> (mkCPRefCPInfo <$> getWord16be)
 
 getCPFieldref :: Get CPInfo
-getCPFieldref = CPFieldref <$> getWord16be <*> getWord16be
+getCPFieldref =
+  CPFieldref
+    <$> (mkCPRefCPInfo <$> getWord16be)
+    <*> (mkCPRefCPInfo <$> getWord16be)
 
 getCPMethodref :: Get CPInfo
-getCPMethodref = CPMethodref <$> getWord16be <*> getWord16be
+getCPMethodref =
+  CPMethodref
+    <$> (mkCPRefCPInfo <$> getWord16be)
+    <*> (mkCPRefCPInfo <$> getWord16be)
 
 getCPString :: Get CPInfo
-getCPString = CPString <$> getWord16be
+getCPString = CPString <$> (mkCPRefCPInfo <$> getWord16be)
 
 getCPNameAndType :: Get CPInfo
-getCPNameAndType = CPNameAndType <$> getWord16be <*> getWord16be
+getCPNameAndType =
+  CPNameAndType
+    <$> (mkCPRefCPInfo <$> getWord16be)
+    <*> (mkCPRefCPInfo <$> getWord16be)
 
 getCPUtf8 :: Get CPInfo
 getCPUtf8 = do
   count <- fromIntegral <$> getWord16be
-  CPUtf8 <$> replicateM count getWord8
+  CPUtf8 <$> (decode <$> replicateM count getWord8)
 
 getCPInfoByTag :: Word8 -> Get CPInfo
 getCPInfoByTag 7 = getCPClass
@@ -76,7 +115,7 @@ type ConstantPool = [CPInfo]
 getConstantPool :: Get ConstantPool
 getConstantPool = do
   count <- (+) (-1 :: Int) . fromIntegral <$> getWord16be
-  replicateM count getCPInfo
+  ([CPUtf8 "Dummy"] ++) <$> replicateM count getCPInfo
 
 data AccessFlag
   = AccAbstract
@@ -95,7 +134,7 @@ data AccessFlag
   | AccSynchronized
   | AccSynthetic
   | AccVarargs
-  deriving (Show)
+  deriving (Show, Eq)
 
 accessFlagMap :: [(Word16, AccessFlag)]
 accessFlagMap =
@@ -127,9 +166,84 @@ getInterfaces = do
   count <- fromIntegral <$> getWord16be
   replicateM count getWord16be
 
-data Attribute = Attribute
-  { attrNameIndex :: Word16
-  , attrInfo :: [Word8]
+data ExceptionTable = ExceptionTable
+  { startPc :: Word16
+  , endPc :: Word16
+  , handlerPc :: Word16
+  , catchType :: Word16
+  } deriving (Show)
+
+getExceptionTable :: Get ExceptionTable
+getExceptionTable =
+  ExceptionTable <$> getWord16be <*> getWord16be <*> getWord16be <*> getWord16be
+
+getExceptionTables :: Get [ExceptionTable]
+getExceptionTables = do
+  count <- fromIntegral <$> getWord16be
+  replicateM count getExceptionTable
+
+type Code = [Word8]
+
+data LineNumberEntry = LineNumberEntry
+  { lineNumEntryStartPc :: Word16
+  , lineNumEntryLineNum :: Word16
+  } deriving (Show)
+
+data AttributeInfo
+  = AttrCode
+      { attrInfoMaxStack :: Word16
+      , attrInfoMaxLocals :: Word16
+      , attrInfoCode :: Code
+      , attrInfoExceptionTables :: [ExceptionTable]
+      , attrInfoAttributes :: [Attribute]
+      }
+  | AttrLineNumberTable
+      { attrLineNumTab :: [LineNumberEntry]
+      }
+  deriving (Show)
+
+isAttrCode :: AttributeInfo -> Bool
+isAttrCode (AttrCode {}) = True
+isAttrCode _ = False
+
+getCode :: Get Code
+getCode = do
+  count <- fromIntegral <$> getWord32be
+  replicateM count getWord8
+
+getLineNumberEntry :: Get LineNumberEntry
+getLineNumberEntry = LineNumberEntry <$> getWord16be <*> getWord16be
+
+getLineNumberTable :: Get AttributeInfo
+getLineNumberTable = do
+  count <- fromIntegral <$> getWord16be
+  AttrLineNumberTable <$> replicateM count getLineNumberEntry
+
+codeFromWords :: [Word8] -> AttributeInfo
+codeFromWords =
+  runGet
+    (AttrCode
+       <$> getWord16be
+       <*> getWord16be
+       <*> getCode
+       <*> getExceptionTables
+       <*> getAttributes)
+    . B.pack
+
+lineNumberTableFromWords :: [Word8] -> AttributeInfo
+lineNumberTableFromWords = runGet getLineNumberTable . B.pack
+
+mkCPRefAttrInfo :: Word16 -> [Word8] -> CPRef AttributeInfo
+mkCPRefAttrInfo nameIdx wrds = f <$> mkCPRefUtf8 nameIdx
+  where
+    f name =
+      case name of
+        "Code" -> codeFromWords wrds
+        "LineNumberTable" -> lineNumberTableFromWords wrds
+        _ -> error $ "Unknown attribute: '" ++ name ++ "'"
+
+newtype Attribute = Attribute
+  { attrInfo :: CPRef AttributeInfo
   } deriving (Show)
 
 getAttribute :: Get Attribute
@@ -137,7 +251,7 @@ getAttribute = do
   nameIdx <- getWord16be
   count <- fromIntegral <$> getWord32be
   info <- replicateM count getWord8
-  return $ Attribute nameIdx info
+  return $ Attribute (mkCPRefAttrInfo nameIdx info)
 
 getAttributes :: Get [Attribute]
 getAttributes = do
@@ -146,13 +260,18 @@ getAttributes = do
 
 data Field = Field
   { fldAccessFlags :: [AccessFlag]
-  , fldNameIndex :: Word16
-  , fldDescIndex :: Word16
+  , fldName :: CPRef String
+  , fldDesc :: CPRef String
   , fldAttributes :: [Attribute]
   } deriving (Show)
 
 getField :: Get Field
-getField = Field <$> getAccessFlags <*> getWord16be <*> getWord16be <*> getAttributes
+getField =
+  Field
+    <$> getAccessFlags
+    <*> (mkCPRefUtf8 <$> getWord16be)
+    <*> (mkCPRefUtf8 <$> getWord16be)
+    <*> getAttributes
 
 getFields :: Get [Field]
 getFields = do
@@ -161,14 +280,18 @@ getFields = do
 
 data Method = Method
   { mthdAccessFlags :: [AccessFlag]
-  , mthdNameIndex :: Word16
-  , mthdDescIndex :: Word16
+  , mthdName :: CPRef String
+  , mthdDesc :: CPRef String
   , mthdAttibutes :: [Attribute]
   } deriving (Show)
 
 getMethod :: Get Method
 getMethod =
-  Method <$> getAccessFlags <*> getWord16be <*> getWord16be <*> getAttributes
+  Method
+    <$> getAccessFlags
+    <*> (mkCPRefUtf8 <$> getWord16be)
+    <*> (mkCPRefUtf8 <$> getWord16be)
+    <*> getAttributes
 
 getMethods :: Get [Method]
 getMethods = do
@@ -207,5 +330,93 @@ parseClassFile = do
   file <- B.readFile "Main.class"
   return $ runGet getClassFile file
 
+getMain :: ClassFile -> Method
+getMain clsf =
+  head
+    . assertWith
+        "Could not find or found too many 'public static main' methods"
+        ((== 1) . length)
+    $ filter ((== "main") . (snd . ($ cpool) . getCPRef . mthdName))
+    $ filter
+        ((\x -> AccStatic `elem` x && AccPublic `elem` x) . mthdAccessFlags)
+    $ clsfMethods clsf
+  where
+    cpool = clsfConstantPool clsf
+
+data Value = ValueCP
+  { valCPInfo :: CPInfo
+  }
+
+type Stack = [Value]
+
+executeMethod :: CPInfo -> StateT (Stack, ConstantPool) Get (IO ())
+executeMethod (CPMethodref cls nat) = do
+  (stack, cpool) <- get
+  let cls' = snd $ getCPRef cls cpool
+  let className = utfString $ snd $ getCPRef (clsName cls') cpool
+  let nat' = snd $ getCPRef nat cpool
+  let methodName = utfString $ snd $ getCPRef (natName nat') cpool
+  let methodType = utfString $ snd $ getCPRef (natDesc nat') cpool
+  case className ++ "." ++ methodName ++ ":" ++ methodType of
+    "java/io/PrintStream.println:(Ljava/lang/String;)V" -> do
+      put (drop 2 stack, cpool)
+      let str = utfString $ snd $ getCPRef (strString $ valCPInfo $ last $ take 2 stack) cpool
+      pure $ putStrLn str
+    _ -> error "Unknown method"
+
+executeMethod _ = error "Supplied reference is not a method"
+
+executeInstruction :: Word8 -> StateT (Stack, ConstantPool) Get (IO ())
+
+-- ldc
+executeInstruction 0x12 = do
+  (stack, cpool) <- get
+  cpIdx <- fromIntegral <$> lift getWord8
+  put (stack ++ [ValueCP $ cpool !! cpIdx], cpool)
+  return $ pure ()
+
+-- getstatic
+executeInstruction 0xb2 = do
+  (stack, cpool) <- get
+  cpIdx <- fromIntegral <$> lift getWord16be
+  put (stack ++ [ValueCP $ cpool !! cpIdx], cpool)
+  return $ pure ()
+
+-- return
+executeInstruction 0xb1 = return $ pure ()
+
+-- invokevirtual
+executeInstruction 0xb6 = do
+  (_, cpool) <- get
+  methodRef <- (cpool !!) . fromIntegral <$> lift getWord16be
+  executeMethod methodRef
+
+executeInstruction ins = error $ "Unknown instruction: '0x" ++ showHex ins "'"
+
+executeCode :: StateT (Stack, ConstantPool) Get [IO ()]
+executeCode = many $ do
+  opcode <- lift getWord8
+  executeInstruction opcode
+
+executeMain :: ClassFile -> [IO ()]
+executeMain clsf =
+  runGet
+    (evalStateT executeCode ([], cpool))
+    (B.pack $ attrInfoCode mainCodeAttr)
+  where
+    cpool = clsfConstantPool clsf
+    mainCodeAttr =
+      snd
+        $ ($ cpool)
+        $ getCPRef
+        $ attrInfo
+        $ head
+        $ assertWith
+            "Could not find or found too many 'Code' attributes for the main method"
+            ((== 1) . length)
+        $ filter (isAttrCode . snd . ($ cpool) . getCPRef . attrInfo)
+        $ mthdAttibutes
+        $ getMain clsf
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = (sequence_ . executeMain) =<< parseClassFile
